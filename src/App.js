48be0f8f4b50c1e6d07ce087610c9e7c4aaf0662
Mike De Groot <mdegroot09@gmail.com>
2019-09-28 11:45:00 -0600

    added router, updated App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,38 @@
 import React from 'react';
-import './App.scss';
+import Navbar from './Components/Navbar/Navbar'
+import Auth from './Components/Auths/Auths'
+import './App.scss'
+import {Provider} from 'react-redux'
+import store from './redux/store'
+import {HashRouter} from 'react-router-dom'
+import router from './router'
 
-function App() {
+export default function App() {
+  // Hide showing dropdown navbar menu on any non-navbar button click
+  let showHideMenu = () => {
+    let showMenu = document.getElementsByClassName('showMenu')[0]
+    let homeMainDiv = document.getElementsByClassName('homeMainDiv')[0]
+    if (showMenu){
+      showMenu.className = 'navMainInit hideMenu'
+      setTimeout(() => {
+        homeMainDiv.style.zIndex = 0
+      }, 250);
+    } 
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        Real Estate App
-      </header>
-    </div>
+    <Provider store={store}>
+      <HashRouter basename='/'>
+        <div className="App">
+          <Navbar/>
+          <div className='loginSuccess'>Login successful</div>
+          <div className='loginSuccess loginFail'>Login unsuccessful</div>
+          <Auth/>
+          <div className='App' onClick={() => showHideMenu()} onTouchMove={() => showHideMenu()}>
+            {router}
+          </div>
+        </div>
+      </HashRouter>
+    </Provider>
   );
 }
-
-export default App;
---
+++ src/router.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import {Switch, Route} from 'react-router-dom'
+import Home from './Components/Home/Home'
+import Post from './Components/PostShow/Post'
+import NewPost from './Components/NewPost/NewPost'
+import EditPost from './Components/EditPost/EditPost'
+
+export default (
+  <Switch>
+    <Route exact path = '/' component={Home} />
+    <Route path = '/post/:id' component={Post} />
+    {/* <Route path='/about' component={About}/> */}
+    <Route path='/newpost' component={NewPost}/>
+    <Route path='/editpost/:id' component={EditPost}/>
+  </Switch>
+)
