4fdc5ca97a13a944bbfab12b6dec748b670fbd9e
Mike De Groot <mdegroot09@gmail.com>
2019-10-01 07:25:24 -0600

    updated titles again

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -4,7 +4,7 @@ export default function Header () {
   return(
     <div className='headerDiv'>
       <img className='headerIcon' src="https://simplejoys.s3.us-east-2.amazonaws.com/simplejoysyellow-1566262672906.png" alt=""/>
-      <div className='headerTitle'>Real Estate App
+      <div className='headerTitle'>Real Estate
         <span id='subtitleFull'>Everything I know</span>
         <span id='subtitlePart1'>Everything I know</span>
         {/* <span id='subtitlePart2'>little things</span> */}
