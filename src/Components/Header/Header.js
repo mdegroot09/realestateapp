e8d90010eb6bce827e5e62276ff3f23d5211f9d9
Mike De Groot <mdegroot09@gmail.com>
2019-10-14 06:58:59 -0600

    removed header logo

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Header () {
   return(
     <div className='headerDiv'>
-      <img className='headerIcon' src="https://simplejoys.s3.us-east-2.amazonaws.com/home-grey-1570541513027.png" alt=""/>
+      {/* <img className='headerIcon' src="https://simplejoys.s3.us-east-2.amazonaws.com/home-grey-1570541513027.png" alt=""/> */}
       <div className='headerTitle'>Real Estate
         <span id='subtitleFull'>with Mike De Groot</span>
         <span id='subtitlePart1'>with Mike De Groot</span>
