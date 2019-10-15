8d3ebac76a85b2af9acac4a6ad5f6ecb82fe119b
Mike De Groot <mdegroot09@gmail.com>
2019-10-15 08:10:44 -0600

    fixed header width bug

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -4,7 +4,7 @@ export default function Header () {
   return(
     <div className='headerDiv' style={{position: 'relative'}}>
       <img className='headerIcon' style={{width: 'calc(100% - 100px)', height: 'auto', maxWidth: '600px'}} src="https://simplejoys.s3.us-east-2.amazonaws.com/logo%20full%20inverse-1571144571531.png" alt=""/>
-      <img className='headerIcon' style={{width: '100vw', height: '250%', position: 'absolute', zIndex: '-10', filter: 'invert(100%)'}} src="https://imageassociatesllc.com/wp-content/themes/oneup-ia/images/blackblur6.png" alt=""/>
+      <img className='headerIcon' style={{width: 'calc(100vw - (100vw - 100%))', height: '250%', position: 'absolute', zIndex: '-10', filter: 'invert(100%)'}} src="https://imageassociatesllc.com/wp-content/themes/oneup-ia/images/blackblur6.png" alt=""/>
     </div>
   )
 }
