f721a60b1e64cada72a0e41c4e424b864e1fdbf4
Mike De Groot <mdegroot09@gmail.com>
2019-10-15 20:23:57 -0600

    updated about layout, styling

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -4,9 +4,9 @@ import Footer from '../Footer/Footer'
 export default class About extends Component {
   render(){
     return(
-      <div style={{paddingTop: '70px', backgroundImage: `url('https://simplejoys.s3.us-east-2.amazonaws.com/neighborhood-1570799839221.jpg')`}}>
+      <div style={{paddingTop: '80px', backgroundImage: `url('https://simplejoys.s3.us-east-2.amazonaws.com/neighborhood-1570799839221.jpg')`}}>
         <div style={{background: 'white', display: 'flex', flexDirection: 'column', justifyContent: 'space-between', alignItems: 'center'}}>
-          <div style={{minHeight: 'calc(100vh - 157px)', width: 'calc(100% - 50px)'}}>
+          <div style={{minHeight: 'calc(100vh - 152px)', width: 'calc(100% - 50px)'}}>
             <h1 className='sectionTitle'>about me</h1>
             <div style={{marginBottom: '50px'}}>
               Real estate and software are my PB&J. Referred to as the trusty realtor, title guy, foreclosure auction 
