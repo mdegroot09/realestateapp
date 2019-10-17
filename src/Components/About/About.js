5dbf31363b316178a051c67d30d71530e43f6070
Mike De Groot <mdegroot09@gmail.com>
2019-10-17 07:22:07 -0600

    updated about component styling

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -4,13 +4,15 @@ import Footer from '../Footer/Footer'
 export default class About extends Component {
   render(){
     return(
-      <div style={{paddingTop: '80px', backgroundImage: `url('https://simplejoys.s3.us-east-2.amazonaws.com/neighborhood-1570799839221.jpg')`}}>
-        <div style={{background: 'white', display: 'flex', flexDirection: 'column', justifyContent: 'space-between', alignItems: 'center'}}>
-          <div style={{minHeight: 'calc(100vh - 152px)', width: 'calc(100% - 50px)'}}>
-            <h1 className='sectionTitle'>about me</h1>
-            <div style={{marginBottom: '50px'}}>
-              Real estate and software are my PB&J. Referred to as the trusty realtor, title guy, foreclosure auction 
-              enthusiast, tax sale crasher, short sale winner, Excel guru, macro-writing magician, and web dev hobbyist...
+      <div style={{paddingTop: '80px', backgroundImage: `url('https://simplejoys.s3.us-east-2.amazonaws.com/neighborhood-1570799839221.jpg')`, width: '100vw'}}>
+        <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'space-between', alignItems: 'center'}}>
+          <div style={{background: 'white', minHeight: 'calc(100vh - 152px)', width: 'calc(100% - 50px)', maxWidth: '800px', display: 'flex', justifyContent: 'center'}}>
+            <div style={{width: 'calc(100% - 50px)', display: 'flex', flexDirection: 'column', alignItems: 'center', textAlign: 'left'}}>
+              <h1 className='sectionTitle'>about me</h1>
+              <div style={{marginBottom: '50px'}}>
+                Real estate and software are my PB&J. Referred to as the trusty realtor, title guy, foreclosure auction 
+                enthusiast, tax sale crasher, short sale winner, Excel guru, macro-writing magician, and web dev hobbyist...
+              </div>
             </div>
           </div>
           <Footer/>
