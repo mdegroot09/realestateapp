eb6eb61611f61d1ff95911537e299e4218b08e76
Mike De Groot <mdegroot09@gmail.com>
2019-10-17 07:37:55 -0600

    added photo to about page

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -9,6 +9,7 @@ export default class About extends Component {
           <div style={{background: 'white', minHeight: 'calc(100vh - 152px)', width: 'calc(100% - 50px)', maxWidth: '800px', display: 'flex', justifyContent: 'center', borderRadius: '5px'}}>
             <div style={{width: 'calc(100% - 50px)', display: 'flex', flexDirection: 'column', alignItems: 'center', textAlign: 'left'}}>
               <h1 className='sectionTitle'>about me</h1>
+              <div className='aboutPhoto'></div>
               <div style={{marginBottom: '50px'}}>
                 Real estate and software are my PB&J. Referred to as the trusty realtor, title guy, foreclosure auction 
                 enthusiast, tax sale crasher, short sale winner, Excel guru, macro-writing magician, and web dev hobbyist...
