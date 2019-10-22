6ed2038551f24209529993cd3b1661b5fcff05dc
Mike De Groot <mdegroot09@gmail.com>
2019-10-22 07:08:34 -0600

    added facebook link

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -5,6 +5,10 @@ export default class Footer extends Component {
     return(
       // <div style={{position: 'bottom'}}>
       <div className='footer'>
+        <img src="https://facebookbrand.com/wp-content/uploads/2019/04/f_logo_RGB-Hex-Blue_512.png" 
+          onClick={() => {window.location.href = 'https://www.facebook.com/mikeadegroot87'}}
+          style={{height: '30px', cursor: 'pointer'}}
+          alt="Link to Mike De Groot's Facebook"/>
         Website custom built by Mike De Groot.
       </div>
     )
