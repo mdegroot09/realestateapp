49c4ba7ba5c71b75ff85f31c4e873b7bf7ca96ac
Mike De Groot <mdegroot09@gmail.com>
2019-10-22 07:22:36 -0600

    added link to GitHub

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -6,14 +6,18 @@ export default class Footer extends Component {
       // <div style={{position: 'bottom'}}>
       <div className='footer'>
         <div className='footerIcons'>
-          <img src="https://facebookbrand.com/wp-content/uploads/2019/04/f_logo_RGB-Hex-Blue_512.png" 
-            onClick={() => {window.location.href = 'https://www.facebook.com/mikeadegroot87'}}
-            style={{height: '30px', cursor: 'pointer'}}
-            alt="Link to Mike De Groot's Facebook"/>
           <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c9/Linkedin.svg/600px-Linkedin.svg.png" 
             onClick={() => {window.location.href = 'https://www.linkedin.com/in/mike-degroot/'}}
             style={{height: '30px', cursor: 'pointer'}}
             alt="Link to Mike De Groot's LinkedIn"/>
+          <img src="https://facebookbrand.com/wp-content/uploads/2019/04/f_logo_RGB-Hex-Blue_512.png" 
+            onClick={() => {window.location.href = 'https://www.facebook.com/mikeadegroot87'}}
+            style={{height: '30px', cursor: 'pointer'}}
+            alt="Link to Mike De Groot's Facebook"/>
+          <img src="https://icon-library.net/images/github-icon-png/github-icon-png-29.jpg" 
+            onClick={() => {window.location.href = 'https://github.com/mdegroot09'}}
+            style={{height: '30px', cursor: 'pointer'}}
+            alt="Link to Mike De Groot's GitHub"/>
         </div>
         Website custom built by Mike De Groot.
       </div>
